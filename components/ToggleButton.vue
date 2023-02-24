9f5acee08c2c27d403e18d47068658a506a96c8a
Oluwafemi21 <olukoyaoluwafemi14@gmail.com>
2023-02-24 04:38:11 +0100

    bug fix

---
+++ components/ToggleButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button
+        class="p-1 w-12 h-5 flex items-center bg-transparent ring-2 ring-neutral-400 rounded-full group focus:bg-emerald-600 focus:ring-emerald-600"
+    >
+        <div
+            class="h-3 w-3 rounded-full bg-neutral-400 group-focus-within:translate-x-7 group-focus-within:bg-black transition-transform duration-300 ease-in-out"
+        ></div>
+    </button>
+</template>
+
+<script setup></script>
+
+<style></style>
