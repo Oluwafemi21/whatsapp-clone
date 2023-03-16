bf421f02327782faa653d2880a2e57f143e60332
Oluwafemi21 <olukoyaoluwafemi14@gmail.com>
2023-03-16 11:11:49 +0100

    fixed checkbox visibility on image selection

--- components/ChatSettings/Media.vue
+++ components/ChatSettings/Media.vue
@@ -3,20 +3,24 @@
         <h3 class="text-xl dark:text-white font-medium">Media</h3>
         <div class="grid grid-cols-3 gap-3 mt-6">
             <div
-                v-for="({ image }, index) in selectImages"
+                v-for="({ image }, index) in imageState"
                 :key="index"
                 class="relative image h-[5.5rem]"
             >
                 <input
                     type="checkbox"
-                    :name="`Image ${i + 1}`"
-                    :id="`Image ${i + 1}`"
-                    class="check absolute top-0.5 z-10 h-5 w-5 right-0.5 focus:outline-none focus:outline-offset-0 focus:ring-transparent focus:ring-0 focus:ring-offset-0 bg-black border-neutral-700 border-2 checked:text-green-700 peer"
+                    :name="`Image ${index + 1}`"
+                    :id="`Image ${index + 1}`"
+                    class="check absolute top-0.5 z-10 h-5 w-5 right-0.5 focus:outline-none focus:outline-offset-0 focus:ring-transparent focus:ring-0 focus:ring-offset-0 bg-black border-neutral-700 border-2 checked:text-green-700 checked:block peer"
+                    :class="{
+                        hidden: !imageState[index].checkBoxVisible,
+                    }"
                     @change="selectImage(index)"
                 />
+
                 <img
                     :src="image"
-                    alt=""
+                    alt="Image not found"
                     class="object-cover hover:opacity-80 w-full z-20 rounded-md peer-checked:border-2 peer-checked:border-green-700"
                 />
             </div>
@@ -25,7 +29,7 @@
 </template>
 
 <script setup>
-const media = [
+const media = ref([
     {
         image: "https://picsum.photos/100",
     },
@@ -36,34 +40,51 @@ const media = [
         image: "https://picsum.photos/100",
     },
     {
-        image: "https://picsum.photos/88",
+        image: "https://picsum.photos/100",
     },
-];
+]);
 
-const selectImages = computed(() => {
-    return media.map((image) => {
+// computed property not updating
+// const selectImages = computed(() => {
+//     return media.value.map((image) => {
+//         return {
+//             ...image,
+//             selected: false,
+//         };
+//     });
+// });
+
+const imageState = useState("images", () => {
+    return media.value.map((image) => {
         return {
             ...image,
             selected: false,
+            checkBoxVisible: false,
         };
     });
 });
+
 const selectImage = (index) => {
-    selectImages.value[index].selected = !selectImages.value[index].selected;
+    imageState.value[index].selected = !imageState.value[index].selected;
+
+    if (imageState.value.some((image) => image.selected)) {
+        imageState.value.map((item) => (item.checkBoxVisible = true));
+    }
+
+    if (!imageState.value.some((image) => image.selected)) {
+        imageState.value.map((item) => (item.checkBoxVisible = false));
+    }
+    console.log(imageState.value);
 };
 </script>
 
 <style scoped>
-.check {
-    display: none;
-}
 .image:hover .check {
     display: block;
     animation: slide-in 200ms ease-in-out forwards;
 }
 
 .image .check:checked {
-    display: block;
     animation: none;
 }
 
