59c3850196f4f6e29c24e14a1eeac438b4a1e574
Oluwafemi21 <olukoyaoluwafemi14@gmail.com>
2023-08-08 07:18:56 +0100

    prodUrl fix

--- composables/getUrl.js
+++ composables/getUrl.js
@@ -1,7 +1,7 @@
 export default function () {
     let url =
-        process?.env?.NUXT_ENV_VERCEL_ENV ?? // Set this to your site URL in production env.
-        process?.env?.NUXT_ENV_VERCEL_URL ?? // Automatically set by Vercel.
+        process.env.NUXT_ENV_VERCEL_ENV ?? // Set this to your site URL in production env.
+        process.env.NUXT_ENV_VERCEL_URL ?? // Automatically set by Vercel.
         "http://localhost:3000/chat/";
     // Make sure to include `https://` when not localhost.
     url = url.includes("http") ? url : `https://${url}`;
