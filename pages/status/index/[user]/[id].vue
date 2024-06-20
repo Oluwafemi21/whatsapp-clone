5d271c1ab86fa93170f9df8b08e91d3c1636ef80
Oluwafemi21 <olukoyaoluwafemi14@gmail.com>
2024-06-20 16:39:56 +0100

    status page fix

--- pages/status/index/[user]/[id].vue
+++ pages/status/index/[user]/[id].vue
@@ -1,17 +1,9 @@
 <template>
-  <section class="min-h-screen grid place-content-center">
-    <p>User - {{ $route.params.user }}</p>
-    <p>Id - {{ $route.params.id }}</p>
-
-    <button
-      class="peer text-white bg-neutral-800 border border-neutral-200 rounded-md py-2 px-3 min-w-[200px] flex items-center justify-between"
-    >
-      <div class="flex items-center gap-2">
-        <Icon name="eva:color-palette-outline" class="shrink-0" />
-        <span class="text-sm">System Default</span>
-      </div>
-      <Icon name="ph:caret-down" />
-    </button>
+  <section
+    class="min-h-screen grid place-content-center text-black dark:text-white"
+  >
+    <p class="text-xl">Coming soon...</p>
+    <button class="underline" @click="$router.back()">Go Back</button>
   </section>
 </template>
 
